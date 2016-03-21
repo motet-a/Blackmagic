746f1006073e710bcb7f88eba52f2b1f5a52ab12
Antoine Motet <motet_a@epitech.eu>
2016-03-21 22:24:26 +0100

    'reel' is French, 'real' is English
    I have added an 'e' to 'complex' too.

--- structure_inheritance.c
+++ structure_inheritance.c
@@ -3,28 +3,28 @@
   Compilation flags : gcc -W -Wall -Wextra -Werror -fms-extentions
   Is it epitech-norm compliant :  Yes
   Source : http://jonathanpoelen.blogspot.fr/2013/01/structure-et-union-anonyme.html
-  Explanations : Anonymous structures that aren't anonymous ! :D Allows you inheritance-like in C, without any functions. 
+  Explanations : Anonymous structures that aren't anonymous ! :D Allows you inheritance-like in C, without any functions.
   Found by grange_c, melis_m
 */
 
 #include <stdio.h>
 
-typedef struct  s_reel
+typedef struct  s_real
 {
   double        r;
-}               t_reel;
+}               t_real;
 
 typedef struct  s_complex
 {
-  t_reel;
+  t_real;
   double        c;
 }               t_complex;
 
 int             main()
 {
-  t_complex     complx;
+  t_complex     complex;
 
-  complx.r = 5;
-  complx.c = 10;
-  printf("Complex is equal to : %.f + %.fi\n", complx.r, complx.c);
+  complex.r = 5;
+  complex.c = 10;
+  printf("Complex is equal to : %.f + %.fi\n", complex.r, complex.c);
 }
