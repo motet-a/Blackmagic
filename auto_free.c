698eb6c4ee482cb28194eff84b79e5203054625d
Benjamin Grange <Arignir@users.noreply.github.com>
2016-03-21 21:52:59 +0000

    Update auto_free.c

--- auto_free.c
+++ auto_free.c
@@ -1,29 +1,31 @@
 /*
 Feature name : auto_free
-Compilation flags : gcc -fplan9-extensions -Wall -Werror
+Compilation flags : gcc -W -Wall -Wextra -Werror
 Is it epitech-norm compliant :  Yes (just remove the printf :D)
 Source : https://gcc.gnu.org/onlinedocs/gcc/Common-Variable-Attributes.html#Common-Variable-Attributes
-Explanations : gives cleanup attribute to variable. this attribute takes a function as parameter which \
-will be called as the varaible gets out of its scope
+Explanations : Gives cleanup attribute to variable. This attribute takes a function as parameter which
+  will be called when the variable gets out of its scope
 Found by Wats0ns/maire_q
 */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 void	free_me(char **to_free)
 {
-  printf("content to free: %s\n", *to_free);
+  printf("Content to free: %s\n", *to_free);
   free(*to_free);
   printf("%s\n", "I'm free !");
 }
 
 int	main()
 {
-  char 	__attribute__ ((cleanup(free_me))) *to_free; //<- Everythin start here...
+  char 	__attribute__ ((cleanup(free_me))) *to_free; //<- Everything starts here...
+  //You may add a macro to make an easier way to set cleanup
+  //It works a bit like alloca, excepts what it doesn't allocate on the stack. Of course, you cannot return a cleanup pointer.
 
-  to_free = malloc(sizeof(char) * 129);
-  to_free[0] = 'a';//<- Some randomo content to prove it is called
-  to_free[2] = '\0';
-  printf("%s\n", "returning from main...");
+  to_free = strdup("Hello World !");
+  printf("%s\n", "Returning from main...");
   return(0);
 }
